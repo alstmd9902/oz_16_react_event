1476f93d54f1ac367f3fed85591c9842e703e9f1
Seungyeon Oh <alstmd9902@naver.com>
2026-02-05 13:16:27 +0900

    design: 글쓰기 버튼 추가 및 레이아웃 패딩 조정

--- local-board/src/shared/Layout.jsx
+++ local-board/src/shared/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <main className="mt-14 flex-1">
+      <main className="mt-14 flex-1 py-12 px-4">
         <Outlet />
       </main>
       <Footer />
