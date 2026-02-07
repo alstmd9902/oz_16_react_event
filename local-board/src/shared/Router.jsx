867bfbbec8534a18de990a6e27a46ef5d5214111
Seungyeon Oh <alstmd9902@naver.com>
2026-02-07 23:12:24 +0900

    chore(router): 라우팅 구조 정리

--- local-board/src/shared/Router.jsx
+++ local-board/src/shared/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "../components/Main";
+import usePosts from "../hooks/usePosts";
 import Login from "../pages/auth/Login";
 import Signup from "../pages/auth/Signup";
 import PostCreate from "../pages/post/PostCreate";
@@ -7,14 +8,18 @@ import PostDetail from "../pages/post/PostDetail";
 import Layout from "./Layout";
 
 function Routers() {
+  const postsState = usePosts();
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
       children: [
-        { index: true, element: <Main /> },
-        { path: "post/create", element: <PostCreate /> }, // 게시글 생성
-        { path: "post/:id", element: <PostDetail /> } // 게시글 상세
+        { index: true, element: <Main posts={postsState.posts} /> },
+        {
+          path: "post/create",
+          element: <PostCreate addPost={postsState.addPost} />
+        }, // 게시글 생성
+        { path: "post/:id", element: <PostDetail posts={postsState.posts} /> } // 게시글 상세
       ]
     },
     { path: "login", element: <Login /> },
