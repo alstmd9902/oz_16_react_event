78ba04af2ee0c0e3423d4c5b93d7cf18c90f5589
Seungyeon Oh <alstmd9902@naver.com>
2026-02-07 23:11:32 +0900

    feat(hooks): 게시글 서버 연동 훅 변경

--- local-board/src/hooks/usePosts.js
+++ local-board/src/hooks/usePosts.js
@@ -1,25 +1,20 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 
-const LOCAL_KEY = "postData"; // key : value
+//서버에 저장된 게시글을 가져오고, 새 게시글 저장을 서버에 요청하는 프론트 관리 훅
+
 export default function usePosts() {
-  const [posts, setPosts] = useState(() => {
-    const saved = localStorage.getItem(LOCAL_KEY);
-    return saved ? JSON.parse(saved) : []; // 로컬은 문자열만 저장 가능하다  JSON.parse -> 역할은 객체 또는 배열로 변환 해주는것이다
-  }); //로컬에 넣을 배열이 필요하다
+  const [posts, setPosts] = useState([]);
 
-  /*
-    1. posts가 바뀐다 -> posts 변경이 되면 로컬스토리지에 저장을 한다 useEffect 사용
-    2. posts 배열을 JSON 문자열로 바꾼다
-    3. LOCAL_KEY를 사용해서 localStorage에 저장한다
-   */
-  const addPost = (post) => {
-    setPosts((prev) => [...prev, post]);
-  };
-
-  //로컬스토리지 기본 문법 : localStorage.setItem(key, value);
   useEffect(() => {
-    localStorage.setItem(LOCAL_KEY, JSON.stringify(posts)); //JSON.stringify -> 문자열로 변환해줌
-  }, [posts]);
+    axios.get("http://localhost:3000/posts").then((res) => setPosts(res.data));
+  }, []);
+
+  //서버 에서 가져오기
+  const addPost = async (formData) => {
+    const res = await axios.post("http://localhost:3000/posts", formData);
+    setPosts((prev) => [...prev, res.data]);
+  };
 
   return { posts, addPost }; // 필요한 값 리턴
 }
